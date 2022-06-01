1e1e2e2e1b8c8ba9abc4d38db35f096905570b0d
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-06-01 15:47:08 +0200

    display category name in product list grid

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -14,6 +14,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
+  currentCategoryName: string;
 
   constructor(private productService: ProductService, 
               private route: ActivatedRoute) { }
@@ -31,9 +32,13 @@ export class ProductListComponent implements OnInit {
     if(hasCategoryId){
       // get the 'id' param string. convert string to a number using the + symbol, symbot '!' for 'null' obj;
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+
+      // get the 'Name' param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!; 
     }else {
       // not category id available ... default to category id = 1
       this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
     //now get the products for the given category id
